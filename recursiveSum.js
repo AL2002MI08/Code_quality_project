86505c6e38743f129dc31cfba090f95ac66f8842
AL2002MI08 <alexandramizero@gmail.com>
2024-08-22 20:07:37 +0200

    function to add numbers in array recursively

--- recursiveSum.js
+++ recursiveSum.js
@@ -1,7 +1,7 @@
 // Implement a JavaScript function to find the sum of all elements in an array recursively.
-function sum(...numbers){
-if(numbers.length > 1) {
-    numbers.length - 1
-    sum()
-}return 
+function sum(numbers){
+    if(numbers.length === 0) {
+        return 0
+    }
+    return numbers[0] + sum(numbers.slice(1))
 }
