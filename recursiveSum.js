f0f85d34424e4196fd958f0665e8e373e7b49db3
AL2002MI08 <alexandramizero@gmail.com>
2024-08-22 19:39:42 +0200

    function to check if number is prime

---
+++ recursiveSum.js
@@ -0,0 +1,7 @@
+// Implement a JavaScript function to find the sum of all elements in an array recursively.
+function sum(...numbers){
+if(numbers.length > 1) {
+    numbers.length - 1
+    sum()
+}return 
+}
