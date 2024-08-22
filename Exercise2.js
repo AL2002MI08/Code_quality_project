4871a9c7c4a8300e86321113fc2ee28d268f197c
AL2002MI08 <alexandramizero@gmail.com>
2024-08-22 17:03:45 +0200

    implement reviews

--- Exercise2.js
+++ Exercise2.js
@@ -1,3 +1,3 @@
 function greet(name) {
     return name ? `Hello, ${name}!`: "Hello!"
-  }
+}
--- Exercise9.js
+++ Exercise9.js
@@ -2,4 +2,3 @@ function calculateMatrixSum(matrix) {
  const sum = matrix.map(number => number.reduce((a, b) => a + b))
  return sum.reduce((acc,val) => acc + val, 0);
 }
-  
