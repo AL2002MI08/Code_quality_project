25d71b1ec11b298fc498d095aa766ad8690d2547
AL2002MI08 <alexandramizero@gmail.com>
2024-08-23 18:13:30 +0200

    implemented changes for flatten array

--- flattenArray.js
+++ flattenArray.js
@@ -1,20 +1,18 @@
 // Write a function(s) that checks the level of nesting of an array.
 //Then, use that function to flatten the array into a single-dimensional array.
-function flattenArray(array) {
-  return array.reduce(
-    (acc, value) => acc.concat(Array.isArray(value) ? flattenArray(value) : value),
-    []
-  );
-}
-function nestLevel(arr){
-    let depth = 0
-    arr.forEach(element => {
-      if(Array.isArray(element)){
-        let currentDepth = nestLevel(element)
-        if(currentDepth + 1 > depth){
-            depth = currentDepth + 1
-        }
-      }  
-    })
-    return depth
+function flatten(array) {
+  let result = [];
+
+  function depth(array) {
+    array.forEach((element) => {
+      if (Array.isArray(element)) {
+        depth(element);
+      } else {
+        result.push(element);
+      }
+    });
+  }
+
+  depth(array);
+  return result;
 }
